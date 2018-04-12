5be3f296c777be209ad047a89a1abe592ac029a6
Carolina Lujan <carolujan_@hotmail.com>
2018-04-12 18:59:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -42,26 +42,18 @@ Plot.post = function post(o) {
 };
 
 var artistToUrl = {
-    'blusa1':'//img.lojasrenner.com.br/item/544343829/zoom/1.jpg',
-    'blusa2':'https://produtos.fotos-riachuelo.com.br/media/catalog/product/cache/image/1098x1370/e9c3970ab036de70892d86c6d221abfe/b/l/blusa-rendada-11839031_foto1_frontal.jpg',
-    'blusa3':'https://img.lojasrenner.com.br/item/544825318/zoom/1.jpg',
-    'blusa4':'https://img.lojasrenner.com.br/item/545665011/zoom/1.jpg',
-    'blusa5':'https://cea.vteximg.com.br/arquivos/ids/2588540-468-560/Camisa-Jeans-Feminina-Manga-Longa-Azul-Escuro-9072661-Azul_Escuro_1.jpg?v=636566277413470000',
-    'blusa6':'https://produtos.fotos-riachuelo.com.br/media/catalog/product/cache/image/1098x1370/e9c3970ab036de70892d86c6d221abfe/b/l/blusa-jeans-manga-7-8-amarracao-12105910_foto1_frontal.jpg',
-    'blusa7':'https://img.lojasrenner.com.br/item/545243635/zoom/1.jpg',
-    'blusa8':'https://cea.vteximg.com.br/arquivos/ids/2309394-468-560/Blusa-Open-Shoulder-com-Guipir-Azul-Marinho-8737561-Azul_Marinho_1.jpg?v=636528511887370000',
-    'blusa9':'https://produtos.fotos-riachuelo.com.br/media/catalog/product/cache/image/1098x1370/e9c3970ab036de70892d86c6d221abfe/a/1/a1_11943220_foto1_frontal.jpg',
-    'blusa10':'https://cea.vteximg.com.br/arquivos/ids/2019546-468-560/Regata-com-Amarracao-Azul-Marinho-8697448-Azul_Marinho_1.jpg?v=636479863061500000',
-    'blusa11':'https://cea.vteximg.com.br/arquivos/ids/2111284-468-560/Blusa-com-Tiras-Azul-8741011-Azul_1.jpg?v=636495624286170000',
-    'blusa12':'https://produtos.fotos-riachuelo.com.br/media/catalog/product/cache/image/1098x1370/e9c3970ab036de70892d86c6d221abfe/b/l/blusa-leve-floral-12088838_foto1_frontal.jpg',
-    'blusa13':'https://img.lojasrenner.com.br/item/544667671/zoom/1.jpg',
-    'blusa14':'https://produtos.fotos-riachuelo.com.br/media/catalog/product/cache/image/1098x1370/e9c3970ab036de70892d86c6d221abfe/b/l/blusa-manga-babados-12283924_foto1_frontal.jpg',
-    'blusa15':'https://img.lojasrenner.com.br/item/544934590/zoom/1.jpg',
-    'blusa16':'https://cea.vteximg.com.br/arquivos/ids/2215744-468-560/Blusa-Ampla-com-Recorte-Azul-Marinho-8954019-Azul_Marinho_1.jpg?v=636512718904430000',
-    'blusa17':'https://produtos.fotos-riachuelo.com.br/media/catalog/product/cache/image/1098x1370/e9c3970ab036de70892d86c6d221abfe/b/l/blusa-recorte-flores-11978414_foto1_frontal.jpg',
-    'blusa18':'https://cea.vteximg.com.br/arquivos/ids/1894687-468-560/Blusa-Open-Shoulder-em-Suede-Azul-Claro-8808199-Azul_Claro_1.jpg?v=636445430678070000',
-    'blusa19':'https://img.lojasrenner.com.br/item/544210217/medium/1.jpg',
-    'blusa20':'https://cea.vteximg.com.br/arquivos/ids/1837802-468-560/Blusa-Ombro-a-Ombro-com-Babado-Azul-8811514-Azul_1.jpg?v=636428203571000000',
+    'blusa1':'https://img.lojasrenner.com.br/item/544492007/zoom/1.jpg',
+    'blusa2':'https://cea.vteximg.com.br/arquivos/ids/2309390-468-560/Blusa-Open-Shoulder-com-Guipir-Vinho-8737561-Vinho_1.jpg?v=636528511713500000',
+    'blusa3':'https://produtos.fotos-riachuelo.com.br/media/catalog/product/cache/image/1098x1370/e9c3970ab036de70892d86c6d221abfe/b/l/blusa-babados-12130176_foto1_frontal.jpg',
+    'blusa4':'https://cea.vteximg.com.br/arquivos/ids/2627553-468-560/Blusa-Feminina-Ciganinha-Manga-Curta-com-Amarracao-Vinho-8921333-Vinho_1.jpg?v=636570694173600000',
+    'blusa5':'https://produtos.fotos-riachuelo.com.br/media/catalog/product/cache/image/1098x1370/e9c3970ab036de70892d86c6d221abfe/b/l/blusa-decote-cut-out-ferragem-12256927_foto1_frontal.jpg',
+    'blusa6':'https://img.lojasrenner.com.br/item/544313814/zoom/1.jpg',
+    'blusa7':'https://cea.vteximg.com.br/arquivos/ids/2770830-468-560/Blusa-Feminina-com-No-Canelada-Manga-Longa-Decote-Redondo-Vinho-9094642-Vinho_1.jpg?v=636585403900700000',
+    'blusa8':'https://produtos.fotos-riachuelo.com.br/media/catalog/product/cache/image/1098x1370/e9c3970ab036de70892d86c6d221abfe/b/l/blusa-manga-sino-12124974_foto1_frontal.jpg',
+    'blusa9':'https://produtos.fotos-riachuelo.com.br/media/catalog/product/cache/image/1098x1370/e9c3970ab036de70892d86c6d221abfe/b/l/blusa-lisa-ilhos-12314641_foto1_frontal.jpg',
+    'blusa10':'https://cea.vteximg.com.br/arquivos/ids/2771063-468-560/Blusa-Feminina--No-More-Fake-Friends--Botone-Assimetrica-Manga-Curta-Decote-Redondo-Vinho-9035459-Vinho_1.jpg?v=636585413773100000',
+    'blusa11':'https://img.lojasrenner.com.br/item/544706546/zoom/1.jpg',
+    'blusa12':'https://img.lojasrenner.com.br/item/544450317/zoom/1.jpg',
 };    
 
 
